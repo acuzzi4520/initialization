c57ae547192658d731c90ec0fbfacb2218288e8c
Student 8 <Student 8@BT_5E7_01-PC>
2016-11-29 14:00:15 -0500

    asdf

--- Period 8 java/src/sortalgo/RecursionIntro.java
+++ Period 8 java/src/sortalgo/RecursionIntro.java
@@ -3,50 +3,70 @@ package sortalgo;
 public class RecursionIntro {
 
 	public static void main(String[] args){
-		System.out.println("Using a for loop:");	
-			for(int i = 0; i < 5; i++){
-				System.out.println("Hello World! x"+i);
-			}
-		System.out.println("Without using a for loop");	
-		forLoop(5,new Action(){
-		
-			private int value = 0;
-			
-			public void act(){
-				System.out.println("Hello World x"+value);
-				value++;
-			}
-		});
+		hanoiSolution(2, "a","b","c");
+//		System.out.println("Using a for loop:");	
+//			for(int i = 0; i < 5; i++){
+//				System.out.println("Hello World! x"+i);
+//			}
+//		System.out.println("Without using a for loop");	
+//		forLoop(5,new Action(){
+//		
+//			private int value = 0;
+//			
+//			public void act(){
+//				System.out.println("Hello World x"+value);
+//				value++;
+//			}
+//		});
 	}
 
-	private static void forLoop(int i, Action action) {
-		if(i <= 0){
-			return;
-		}else{
-			action.act();
-			forLoop(i-1, action);
-		}
-	}
-	
-	private static int factorial(int x){
-		
-//		//for loop method
-		int factorial = 1;
-//		for(int i = x; i > 0; i--){
-//			factorial = factorial*i; 
+//	private static void forLoop(int i, Action action) {
+//		if(i <= 0){
+//			return;
+//		}else{
+//			action.act();
+//			forLoop(i-1, action);
 //		}
-//		System.out.println("factorial");
-//		return factorial;
+//	}
+//	
+//	private static int factorial(int x){
+//		
+////		//for loop method
+//		int factorial = 1;
+////		for(int i = x; i > 0; i--){
+////			factorial = factorial*i; 
+////		}
+////		System.out.println("factorial");
+////		return factorial;
+//		
+//		//recursion method
+//		
+//		
+//		
+//		//Recursion method
+//		  
+//		  if(x > 1){
+//		  	return x*factorial(x-1);
+//		  }
+//		  return 1;
+//	}
+	
+	public static void hanoiSolution(int numberOfDiscs,
+			String startPeg,
+			String midPeg,
+			String endPeg){
 		
-		//recursion method
-		
-		
-		
-		//Recursion method
-		  
-		  if(x > 1){
-		  	return x*factorial(x-1);
-		  }
-		  return 1;
+		if(numberOfDiscs >= 1){
+			System.out.println("Move "+ startPeg + " to "+ endPeg);
+		}else{
+			hanoiSolution(numberOfDiscs-1, 
+					startPeg,
+					endPeg,
+					midPeg);
+			hanoiSolution(1, 
+					startPeg,
+					midPeg,
+					endPeg);
+		}
 	}
 }
