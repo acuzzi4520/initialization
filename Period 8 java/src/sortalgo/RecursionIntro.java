3196f41126c487da323639086b1f824d49066570
Student 8 <Student 8@BT_5E7_01-PC>
2016-11-29 13:41:34 -0500

    asdf

--- Period 8 java/src/sortalgo/RecursionIntro.java
+++ Period 8 java/src/sortalgo/RecursionIntro.java
@@ -3,16 +3,50 @@ package sortalgo;
 public class RecursionIntro {
 
 	public static void main(String[] args){
-	System.out.println("Using a for loop:");	
-		for(int i = 0; i < 5; i++){
-			System.out.println("Hello World!");
-		}
-	System.out.println("Without using a for loop");	
-	forLoop(5,new Action(){
-		public void act(){
-			System.out.println("Hello World");
-		}
-	});
+		System.out.println("Using a for loop:");	
+			for(int i = 0; i < 5; i++){
+				System.out.println("Hello World! x"+i);
+			}
+		System.out.println("Without using a for loop");	
+		forLoop(5,new Action(){
+		
+			private int value = 0;
+			
+			public void act(){
+				System.out.println("Hello World x"+value);
+				value++;
+			}
+		});
 	}
 
+	private static void forLoop(int i, Action action) {
+		if(i <= 0){
+			return;
+		}else{
+			action.act();
+			forLoop(i-1, action);
+		}
+	}
+	
+	private static int factorial(int x){
+		
+//		//for loop method
+		int factorial = 1;
+//		for(int i = x; i > 0; i--){
+//			factorial = factorial*i; 
+//		}
+//		System.out.println("factorial");
+//		return factorial;
+		
+		//recursion method
+		
+		
+		
+		//Recursion method
+		  
+		  if(x > 1){
+		  	return x*factorial(x-1);
+		  }
+		  return 1;
+	}
 }
