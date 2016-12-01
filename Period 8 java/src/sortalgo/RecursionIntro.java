2ecfb88a96d56ff5fdd16eea0af1521ea3befa97
Student 8 <Student 8@BT_5E7_01-PC>
2016-12-01 13:59:52 -0500

    asdf

--- Period 8 java/src/sortalgo/RecursionIntro.java
+++ Period 8 java/src/sortalgo/RecursionIntro.java
@@ -3,7 +3,9 @@ package sortalgo;
 public class RecursionIntro {
 
 	public static void main(String[] args){
-		hanoiSolution(3, "a","b","c");
+		int n = 25;
+		System.out.println("The "+n+"th number of the Fibonacci sequence is "+fibonacci(n));
+//	    hanoiSolution(3, "a","b","c");
 //		System.out.println("Using a for loop:");	
 //			for(int i = 0; i < 5; i++){
 //				System.out.println("Hello World! x"+i);
@@ -18,6 +20,18 @@ public class RecursionIntro {
 //				value++;
 //			}
 //		});
+	}
+
+private static int fibonacci(int n) {
+		if(n <= 1){
+			return 1;
+		}else{
+			int previous = fibonacci(n-1);
+			print("before we find fibonacci "+n+" we need to "
+					+ "find fibonacci "+(n-1)+", which is "+previous);
+			int beforeprevious = fibonacci(n-2);
+			return previous+beforeprevious;
+		}
 	}
 
 //	private static void forLoop(int i, Action action) {
@@ -50,18 +64,29 @@ public class RecursionIntro {
 //		  }
 //		  return 1;
 //	}
+	private static int count = 1;
 	
-	public static void hanoiSolution(int numberOfDiscs,
-			String startPeg,
-			String midPeg,
-			String endPeg){
-		
-		if(numberOfDiscs >= 1){
-			System.out.println("Move "+ startPeg + " to "+ endPeg);
-		}else{
-			hanoiSolution(numberOfDiscs-1, startPeg, endPeg, midPeg);
-			hanoiSolution(1, startPeg, midPeg,endPeg);
-			hanoiSolution(numberOfDiscs-1, midPeg, startPeg, endPeg);
-		}
+	public static void print(String s){
+		System.out.println("Move #"+count+": "+ s);
+		count++;
 	}
+	
+//	public static void hanoiSolution(int numberOfDiscs,
+//			String startPeg,
+//			String midPeg,
+//			String endPeg){
+//		
+//		if(numberOfDiscs <= 1){
+//			print("Move "+ startPeg + " to "+ endPeg);
+//		}else{
+//			System.out.println("In order to move "+
+//					numberOfDiscs+
+//					" over to peg "+ endPeg +" We must move "+ 
+//					(numberOfDiscs - 1)+" over to peg "+midPeg+
+//					" first.");
+//			hanoiSolution(numberOfDiscs-1, startPeg, endPeg, midPeg);
+//			hanoiSolution(1, startPeg, midPeg,endPeg);
+//			hanoiSolution(numberOfDiscs-1, midPeg, startPeg, endPeg);
+//		}
+//	}
 }
