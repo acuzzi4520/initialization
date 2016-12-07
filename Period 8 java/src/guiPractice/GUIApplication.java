b407aade776a5d6237d6082b4c8834af22fbd447
Student 8 <Student 8@BT_5E7_01-PC>
2016-12-07 14:02:58 -0500

    asdf

--- Period 8 java/src/guiPractice/GUIApplication.java
+++ Period 8 java/src/guiPractice/GUIApplication.java
@@ -1,8 +1,14 @@
 package guiPractice;
 
+import java.awt.Graphics;
+
 import javax.swing.JFrame;
 
 public class GUIApplication extends JFrame{
+	
+	private Screen currentScreen;
+
+	
 	//main method for practice only
 	public static void main(String[] args) {
 		new GUIApplication();
@@ -19,7 +25,20 @@ public class GUIApplication extends JFrame{
 		int width = 600;
 		int height = 400;
 		setBounds( x, y, width, height);
+		initScreen();
 		setVisible(true);
+	}
+
+
+
+
+	protected void initScreen() {
+		Screen startScreen = new Screen(getWidth(), getHeight());
+		currentScreen = startScreen;
+	}
+	
+	public void paint(Graphics g){
+		g.drawImage(currentScreen.getImage(), 0, 0, null);
 	}
 
 
