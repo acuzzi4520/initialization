3b0451718f62b6239866f1561f82e7f3ba326976
Student 8 <Student 8@BT_5E7_01-PC>
2016-12-07 13:58:51 -0500

    asdf

--- Period 8 java/src/guiPractice/GUIApplication.java
+++ Period 8 java/src/guiPractice/GUIApplication.java
@@ -1,16 +1,28 @@
 package guiPractice;
 
-public class GUIApplication {
+import javax.swing.JFrame;
 
+public class GUIApplication extends JFrame{
 	//main method for practice only
 	public static void main(String[] args) {
-		
+		new GUIApplication();
 		
 	}
 	
+	
+	
+	
 	public GUIApplication() {
-		// TODO Auto-generated constructor stub
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		int x = 40;
+		int y = 40;
+		int width = 600;
+		int height = 400;
+		setBounds( x, y, width, height);
+		setVisible(true);
 	}
 
 
+	
+	
 }
