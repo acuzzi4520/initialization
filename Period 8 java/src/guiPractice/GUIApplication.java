483b65ba19b88b0fa0a60333132cfee5a4a95097
Student 8 <Student 8@BT_5E7_01-PC>
2016-12-07 13:45:28 -0500

    cvn

---
+++ Period 8 java/src/guiPractice/GUIApplication.java
@@ -0,0 +1,16 @@
+package guiPractice;
+
+public class GUIApplication {
+
+	//main method for practice only
+	public static void main(String[] args) {
+		
+		
+	}
+	
+	public GUIApplication() {
+		// TODO Auto-generated constructor stub
+	}
+
+
+}
