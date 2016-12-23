98507e8ad2d443537792f95888712b8cb52eab86
Student 8 <Student 8@BT_5E7_01-PC>
2016-12-23 13:47:22 -0500

    asdf

---
+++ Period 8 java/src/guiPractice/WhackAMole/Mole.java
@@ -0,0 +1,75 @@
+package guiPractice.WhackAMole;
+
+import java.util.List;
+
+import guiPractice.components.Action;
+import guiPractice.components.Visible;
+import guiPractice.sampleGames.ClickableSceen;
+
+public class Mole extends ClickableSceen implements MoleInterface{
+
+	public Mole() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean isHovered(int x, int y) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void act() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getX() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getY() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isAnimated() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int getAppearenceTime() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setAppearenceTime(int d) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setAction(Action action) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void initAllObjects(List<Visible> viewObjects) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void initObjects(List<Visible> viewObjects) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ Period 8 java/src/guiPractice/WhackAMole/Player.java
@@ -0,0 +1,25 @@
+package guiPractice.WhackAMole;
+
+import java.awt.Graphics2D;
+
+import guiPractice.components.Component;
+
+public class Player extends Component implements PlayerInterface{
+
+	public Player() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void increaseScore(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Graphics2D g) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
