b173d0b43f5e483346d81caa48e5d1033c881d76
Student 8 <Student 8@BT_5E7_01-PC>
2017-01-05 13:17:44 -0500

    asdf

--- Period 8 java/src/simonSays/ButtonInterfaceAnthonyCuzzi.java
+++ Period 8 java/src/simonSays/ButtonInterfaceAnthonyCuzzi.java
@@ -1,7 +1,18 @@
 package simonSays;
 
+import java.awt.Color;
+
+import guiPractice.components.Action;
 import guiPractice.components.Clickable;
 
 public interface ButtonInterfaceAnthonyCuzzi extends Clickable{
+
+	void setColor(Color color);
+
+	void setX(int i);
+
+	void setY(int i);
+	
+	void setAction(Action a);
 
 }
--- Period 8 java/src/simonSays/SimonScreenAnthonyCuzzi.java
+++ Period 8 java/src/simonSays/SimonScreenAnthonyCuzzi.java
@@ -1,8 +1,11 @@
 package simonSays;
 
+import java.awt.Color;
 import java.util.ArrayList;
 import java.util.List;
 
+import guiPractice.components.Action;
+import guiPractice.components.Button;
 import guiPractice.components.TextLabel;
 import guiPractice.components.Visible;
 import guiPractice.sampleGames.ClickableScreen;
@@ -45,7 +48,23 @@ public class SimonScreenAnthonyCuzzi extends ClickableScreen implements Runnable
 	}
 
 	private MoveInterfaceAnthonyCuzzi randomMove() {
-		button[(int) ((int)1 + (Math.random()*6))] = ;
+		ButtonInterfaceAnthonyCuzzi b = null;
+		int rdm = (int)(1 + (Math.random()*6));
+		if(rdm == lastSelectedButton){
+			while(rdm == lastSelectedButton){
+				rdm = (int)(1 + (Math.random()*6));
+			}
+			b = (ButtonInterfaceAnthonyCuzzi) button[rdm];
+		}
+		return getMove(b);
+	}
+	
+	/**
+	Placeholder until partner finishes creation of Move class
+	*/
+	private MoveInterfaceAnthonyCuzzi getMove(ButtonInterfaceAnthonyCuzzi b) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 	/**
@@ -57,8 +76,38 @@ public class SimonScreenAnthonyCuzzi extends ClickableScreen implements Runnable
 	}
 
 	private void addButtons() {
-		
-		
+		int numberOfButtons = 6;
+		Color[] Colors = {Color.red,Color.blue,Color.green,Color.orange,Color.yellow,Color.magenta};
+		for(int i = 0; i < numberOfButtons; i++){
+			 ButtonInterfaceAnthonyCuzzi b = getAButton();
+			 b.setColor(Colors[i]); 
+			    b.setX(60 + i*20);
+			    b.setY(60);
+			    b.setAction(new Action(){
+
+			    	public void act(){
+			    		if(acceptingInput){
+			    			Thread blink = new Thread(new Runnable(){
+
+			    				public void run(){
+			    				}
+
+			    				});
+			    		}
+			    	}
+
+			    	}); 
+		}
+	}
+	
+	/**
+	Placeholder until partner finishes creation of Button class
+	 * @return 
+	*/
+
+	private ButtonInterfaceAnthonyCuzzi getAButton() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 	@Override
